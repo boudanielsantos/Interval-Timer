f38fccb898f91b6c98335685fadcb463168aa193
boudanielsantos <bousantos@gmail.com>
2026-01-13 14:28:35 +0800

    Added initial UI of SavedScreen

--- app/src/main/java/com/example/intervaltimer/screens/save/SavedScreen.kt
+++ app/src/main/java/com/example/intervaltimer/screens/save/SavedScreen.kt
@@ -1,15 +1,38 @@
 package com.example.intervaltimer.screens.save
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.intervaltimer.model.IntervalTimer
 
@@ -42,5 +65,102 @@ fun ShowNoSavedIntervals() {
 @Composable
 fun SavedContent(intervals: List<IntervalTimer>) {
 
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items(items = intervals) { interval ->
+            IntervalItem(interval)
+        }
+    }
 
+}
+
+@Composable
+@Preview
+fun IntervalItem(
+    interval: IntervalTimer = IntervalTimer(
+        name = "Interval 1",
+        workCountMinute = 20,
+        workCountSecond = 30,
+        restCountMinute = 20,
+        restCountSecond = 30,
+        sets = 5
+    )
+) {
+    var showMenu by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .size(200.dp)
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Row(
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+            ) {
+                IconButton(onClick = { showMenu = !showMenu }) {
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = "More Options"
+                    )
+                }
+                DropdownMenu(
+                    expanded = showMenu,
+                    onDismissRequest = { showMenu = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("Edit") },
+                        onClick = { }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Delete") },
+                        onClick = { }
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .clickable {
+
+                    }
+                    .padding(bottom = 8.dp, end = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = "Play Saved Workout Icon"
+                    )
+                }
+                Text("Start")
+            }
+            Column() {
+                Text(interval.name, style = MaterialTheme.typography.headlineLarge)
+
+                Text(
+                    "Sets :     ${interval.sets}x", style = MaterialTheme.typography.labelLarge,
+                    modifier = Modifier.padding(8.dp)
+                )
+                Text(
+                    "WORK :  ${interval.workCountMinute} : ${interval.workCountSecond}",
+                    style = MaterialTheme.typography.labelLarge,
+                    modifier = Modifier.padding(8.dp)
+                )
+                Text(
+                    "REST :    ${interval.restCountMinute} : ${interval.restCountSecond}",
+                    style = MaterialTheme.typography.labelLarge, modifier = Modifier.padding(8.dp)
+                )
+
+
+            }
+        }
+
+
+    }
 }
